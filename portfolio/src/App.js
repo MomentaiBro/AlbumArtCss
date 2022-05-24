e5fa60cc59a2a3f0e1ab1c6293e8ceb698a7d8d3
Alexander Sherwin <alx.sherwin@gmail.com>
2022-05-24 09:14:54 -0600

    added initial styling for navbar going to switch to a new branch for styling tho

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
-import  {RPCV3}  from './artwork/Artists/Macroblank/RarePsalmsCollectionVol3/RarePsalmsCollectionVol3';
-import {NavBar} from './components/NavBar/NavBar';
+import  { RPCV3 }  from './artwork/Artists/Macroblank/RarePsalmsCollectionVol3/RarePsalmsCollectionVol3';
+import { Display } from './components/Display/Display';
+import { NavBar } from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <h1>Welcome to my portfolio of Ablum Covers made with CSS</h1>
+      <Display />
       {/* <RPCV3 /> */}
     </div>
   );
