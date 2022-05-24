89d58fa1c4369ac85f56af606a5ddc320873db03
Alexander Sherwin <alx.sherwin@gmail.com>
2022-05-24 10:58:09 -0600

    redoing css styling so that im following good practice

--- portfolio/src/components/NavBar/NavBar.js
+++ portfolio/src/components/NavBar/NavBar.js
@@ -5,8 +5,16 @@ export const NavBar = () => {
   return (
     <div className='NavBar'>
     
-        <h1>Welcome to my Artwork</h1>
-        NavBar
+        <div className='title'>
+          Title
+        </div>
+        <nav>
+          <ul>
+            <li><a href="">One</a></li>
+            <li><a href="">Two</a></li>
+            <li><a href="">Three</a></li>
+          </ul>
+        </nav>
     
     </div>
   )
