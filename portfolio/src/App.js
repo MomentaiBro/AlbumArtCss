db5103019cf46f5c12b9a35e334f14396312a7fd
Alexander Sherwin <alx.sherwin@gmail.com>
2022-05-24 09:08:02 -0600

    create navbar component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import { RPCV3 } from './artwork/Artists/Macroblank/RarePsalmsCollectionVol3/RarePsalmsCollectionVol3';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <h1>Welcome to my portfolio of Ablum Covers made with CSS</h1>
-      <RPCV3 />
+      {/* <RPCV3 /> */}
     </div>
   );
 }
